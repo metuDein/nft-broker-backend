f07d343b8421f936abd73f85ab72967f9512ecec
metu dein <maxmetadein@gmail.com>
2023-06-04 20:06:53 +0100

    update

--- config/whiteList.js
+++ config/whiteList.js
@@ -1,8 +1,8 @@
 const whiteList = [
     'http://localhost:3000',
     'https://tritonprime.onrender.com',
-    'http://tritonsprime.io/',
-    'https://tritonsprime.io/'
+    'http://tritonsprime.io',
+    'https://tritonsprime.io'
 ]
 
 module.exports = whiteList;
