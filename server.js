7d669da888646e5fa8fb891b7bf1a8a293dac582
metudein <maxmetadein@gmail.com>
2023-06-11 22:04:23 +0100

    latest commit

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(cors(corsOptions))
 app.use(cookieParser())
 
 
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({extended : false}));
 app.use(express.json())
 app.use('/', express.static(join(__dirname, '/public')))
 
